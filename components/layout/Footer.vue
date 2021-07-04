d19444967373f4aa6801f3a64ff3616d9d01e843
Jun-Sik Choi <leichoi91@gmail.com>
2021-07-05 01:54:24 +0900

    WIP: Layout Designing

---
+++ components/layout/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="footer">
+    {{ `&copy; ${new Date().getFullYear()} Jun-Sik Choi` }}
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.footer {
+  /* height: 2rem; */
+  margin: 1rem 0;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrapper">
+    <Header />
+    <nuxt class="contents" />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from "../components/layout/Footer.vue";
+import Header from "../components/layout/Header.vue";
+export default {
+  components: { Header, Footer }
+};
+</script>
+
+<style scoped lang="scss">
+.wrapper {
+  max-width: 900px;
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.contents {
+  min-height: 100vh;
+}
+</style>
