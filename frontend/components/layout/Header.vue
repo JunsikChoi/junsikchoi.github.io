58bbc15727d4f2f74a8d9636af9f8fcd4470860a
Jun-Sik Choi <leichoi91@gmail.com>
2021-07-05 01:54:24 +0900

    WIP: Layout Designing

---
+++ frontend/components/layout/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <header class="header">
+    <div class="header__logo">
+      JUNSIK.DEV
+    </div>
+    <ul class="header__menu">
+      <li class="header__menu__item">
+        About
+      </li>
+      <li class="header__menu__item">
+        Blog
+      </li>
+      <li class="header__menu__item">
+        Projects
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+.header {
+  width: 100%;
+  height: 4rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 2rem;
+  &__menu {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+    &__item {
+      margin-left: 2rem;
+    }
+  }
+}
+
+@media screen and (max-width: 900px) {
+  .header {
+    margin: 0 1rem;
+  }
+}
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,17 @@
 <template>
-  <Tutorial/>
+  <div class="test">
+    hi
+  </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
+
+<style lang="scss" scoped>
+.test {
+  /* width: 100px; */
+  /* height: 1000px; */
+  /* background-color: red; */
+}
+</style>
